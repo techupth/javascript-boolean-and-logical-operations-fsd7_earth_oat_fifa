2d1eaee78688ba23057d183ea7c5f79d4d8ed454
Thanakon <thanakon.piti@gmail.com>
2024-09-30 14:43:00 +0700

    feat: boolean and logical operation on ex1-3

--- ex-1.js
+++ ex-1.js
@@ -4,5 +4,8 @@ let hasCriminalBlacklist;
 let isAllow;
 
 // Start coding here
-
+isOver18 = true;
+hasCriminalBlacklist = false;
+isAllow = true;
+isAllow = (isOver18 => 18) && !hasCriminalBlacklist;
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -7,6 +7,16 @@ let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
 
-let hasPromotion;
+lastMonthPaidMoreThan4000 = 4001;
+isWeekday = true;
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = false;
+let hasPromotion =
+  ((lastMonthPaidMoreThan4000) => 4000) &&
+  isWeekday &&
+  !hasBoughtProductFromITCategory &&
+  !hasAttendedDiscountEvent &&
+  isPlatinum;
 
 console.log(hasPromotion);
